0f1d562e83e0a4bb151d3d7fa0fc812c465cc2d8
Crowdbotics <engineering@crowdbotics.com>
2023-03-26 00:19:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-breeze-dev-72109",
+  url: "https://white-breeze-dev-72109.botics.co",
+  api: "https://white-breeze-dev-72109.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
